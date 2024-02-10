6b1c701fe7e79f781c5c646f13563116c07264eb
Sanyal Ritabrata <rito@Sanyals-MacBook-Air.local>
2024-02-10 12:13:46 +0100

    create new sampling callback, move sampling at epoch end from ddpm_module to SamplingCallback

--- src/callbacks/sampling_callback.py
+++ src/callbacks/sampling_callback.py
@@ -1,38 +1,51 @@
-'''
 import torch
 import lightning as L
 from matplotlib import pyplot as plt
+from ..models.utils.ddpm_utils import get_tensor_image, get_index_from_list
+import os
+
 
 class SamplingCallback(L.Callback):
+    '''
+    samples noise and uses trained model to predict less noisy images
+    '''
 
-    def __init__(self,img_size) -> None:
+    def __init__(self,img_size:int, out:str) -> None:
         super().__init__()
         self.img_size = img_size
-    
-    #samples noise and uses trained model to predict less noisy images
-    
-    def on_train_epoch_end(self, trainer, pl_module):
+        self.out = out
+
+
+    def on_train_epoch_end(self, trainer: L.Trainer, pl_module:L.LightningModule) -> None:
         # do something with all training_step outputs, for example:
-        self.sample_plot_image(pl_module, ep=trainer.current_epoch,step=trainer.global_step)
+        self.sample_plot_image(pl_module)
+
 
     @torch.no_grad()
-    def sample_timestep(pl_module, x, t):
+    def sample_timestep(self, x:torch.Tensor, t:torch.Tensor, **kwargs) -> None:
         """
         Calls the model to predict the noise in the image and returns
         the denoised image.
         Applies noise to this image, if we are not in the last step yet.
         """
-        betas_t = pl_module.get_index_from_list(pl_module.betas, t, x.shape)
+        betas = kwargs['betas']
+        device = kwargs['device']
+        sqrt_one_minus_alphas_cumprod  = kwargs['sqrt_one_minus_alphas_cumprod']
+        sqrt_recip_alphas = kwargs['sqrt_recip_alphas']
+        net = kwargs['net']
+        posterior_variance = kwargs['posterior_variance']
+
+        betas_t = get_index_from_list(betas, t, x.shape,device)
         sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
-            sqrt_one_minus_alphas_cumprod, t, x.shape
+            sqrt_one_minus_alphas_cumprod, t, x.shape, device
         )
-        sqrt_recip_alphas_t = get_index_from_list(sqrt_recip_alphas, t, x.shape)
+        sqrt_recip_alphas_t = get_index_from_list(sqrt_recip_alphas, t, x.shape,device)
 
         # Call model (current image - noise prediction)
         model_mean = sqrt_recip_alphas_t * (
-            x - betas_t * pl_module.net(x, t) / sqrt_one_minus_alphas_cumprod_t
+            x - betas_t * net(x, t) / sqrt_one_minus_alphas_cumprod_t
         )
-        posterior_variance_t = get_index_from_list(posterior_variance, t, x.shape)
+        posterior_variance_t = get_index_from_list(posterior_variance, t, x.shape,device)
 
         if t == 0:
             # As pointed out by Luis Pereira (see YouTube comment)
@@ -41,27 +54,32 @@ class SamplingCallback(L.Callback):
         else:
             noise = torch.randn_like(x)
             return model_mean + torch.sqrt(posterior_variance_t) * noise
-    
+
+
     @torch.no_grad()
-    def sample_plot_image(self,pl_module):
+    def sample_plot_image(self, pl_module:L.LightningModule) -> None:
         # Sample noise
-        img = torch.randn((1, 3, img_size, img_size), device=device)
+        img = torch.randn((1, 3, self.img_size, self.img_size), device=pl_module.device)
         plt.figure(figsize=(15,15))
         plt.axis('off')
         num_images = 10
-        stepsize = int(T/num_images)
+        stepsize = int(pl_module.T/num_images)
 
-        for i in range(0,T)[::-1]:
-            t = torch.full((1,), i, device=device, dtype=torch.long)
-            img = sample_timestep(model, img, t)
+        plmodule_data = dict(betas=pl_module.betas,device=pl_module.device,\
+                             sqrt_one_minus_alphas_cumprod=pl_module.sqrt_one_minus_alphas_cumprod,\
+                                sqrt_recip_alphas=pl_module.sqrt_recip_alphas,net=pl_module.net,\
+                                    posterior_variance=pl_module.posterior_variance)
+
+        for i in range(0,pl_module.T)[::-1]:
+            t = torch.full((1,), i, device=pl_module.device, dtype=torch.long)
+            img = self.sample_timestep(img, t, **plmodule_data)
             # Edit: This is to maintain the natural range of the distribution
             img = torch.clamp(img, -1.0, 1.0)
             if i % stepsize == 0:
                 plt.subplot(1, num_images, int(i/stepsize)+1)
-                less_noisy_img = show_tensor_image(img.detach().cpu())
+                less_noisy_img = get_tensor_image(img.detach().cpu())
                 plt.imshow(less_noisy_img)
 
 
-        plt.savefig(f'epoch-{ep}-step-{step}.png',dpi=200,bbox_inches="tight")
+        plt.savefig(os.path.join(self.out,f'epoch-{pl_module.current_epoch}.png'),dpi=200,bbox_inches="tight")
         plt.close()
-'''
--- src/models/ddpm_module.py
+++ src/models/ddpm_module.py
@@ -8,7 +8,6 @@ import torch.nn.functional as F
 
 from matplotlib import pyplot as plt
 from .utils.ddpm_utils import get_tensor_image, get_index_from_list
-import os
 
 class DDPMLitModule(LightningModule):
     """Example of a `LightningModule` for MNIST classification.
@@ -49,9 +48,7 @@ class DDPMLitModule(LightningModule):
         optimizer: torch.optim.Optimizer,
         scheduler: torch.optim.lr_scheduler,
         compile: bool,
-        out: str,
         T:int=300,
-        img_size:int=64
     ) -> None:
         """Initialize a `DDPMLitModule`.
 
@@ -67,8 +64,6 @@ class DDPMLitModule(LightningModule):
 
         self.net = net
         self.T = T
-        self.img_size = img_size
-        self.out = out
 
         # Pre-calculate different terms for closed form
         self.betas = self.linear_beta_schedule_(timesteps=T)
@@ -164,59 +159,7 @@ class DDPMLitModule(LightningModule):
 
     def on_train_epoch_end(self) -> None:
         #"Lightning hook that is called when a training epoch ends.
-        self.sample_plot_image()
-
-
-    @torch.no_grad()
-    def sample_timestep(self, x, t):
-        """
-        Calls the model to predict the noise in the image and returns
-        the denoised image.
-        Applies noise to this image, if we are not in the last step yet.
-        """
-        betas_t = get_index_from_list(self.betas, t, x.shape,self.device)
-        sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
-            self.sqrt_one_minus_alphas_cumprod, t, x.shape, self.device
-        )
-        sqrt_recip_alphas_t = get_index_from_list(self.sqrt_recip_alphas, t, x.shape,self.device)
-
-        # Call model (current image - noise prediction)
-        model_mean = sqrt_recip_alphas_t * (
-            x - betas_t * self.net(x, t) / sqrt_one_minus_alphas_cumprod_t
-        )
-        posterior_variance_t = get_index_from_list(self.posterior_variance, t, x.shape,self.device)
-
-        if t == 0:
-            # As pointed out by Luis Pereira (see YouTube comment)
-            # The t's are offset from the t's in the paper
-            return model_mean
-        else:
-            noise = torch.randn_like(x)
-            return model_mean + torch.sqrt(posterior_variance_t) * noise
-
-
-    @torch.no_grad()
-    def sample_plot_image(self):
-        # Sample noise
-        img = torch.randn((1, 3, self.img_size, self.img_size), device=self.device)
-        plt.figure(figsize=(15,15))
-        plt.axis('off')
-        num_images = 10
-        stepsize = int(self.T/num_images)
-
-        for i in range(0,self.T)[::-1]:
-            t = torch.full((1,), i, device=self.device, dtype=torch.long)
-            img = self.sample_timestep(img, t)
-            # Edit: This is to maintain the natural range of the distribution
-            img = torch.clamp(img, -1.0, 1.0)
-            if i % stepsize == 0:
-                plt.subplot(1, num_images, int(i/stepsize)+1)
-                less_noisy_img = get_tensor_image(img.detach().cpu())
-                plt.imshow(less_noisy_img)
-
-
-        plt.savefig(os.path.join(self.out,f'epoch-{self.current_epoch}.png'),dpi=200,bbox_inches="tight")
-        plt.close()
+        pass
 
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
@@ -240,9 +183,10 @@ class DDPMLitModule(LightningModule):
         :param batch_idx: The index of the current batch.
         """
         loss = self.model_step(batch)
-
-        # update and log metrics
         self.test_loss(loss)
+
+        self.log("test/loss", self.test_loss, prog_bar=True)
+        # update and log metrics
 
 
     def on_test_epoch_end(self) -> None:
